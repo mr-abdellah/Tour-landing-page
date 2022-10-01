30bce60cbea690569dd141de5c251fcbaecb6af7
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-01 14:32:06 +0100

    Add responsive content section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import heroImg from "../assets/hero.svg";
+import section2Img from '../assets/section2.svg'
 import '../styles/homepage.css'
 
 const Homepage = () => {
@@ -27,7 +28,20 @@ const Homepage = () => {
                     </form>
                     <button>Explore</button>
                 </div>
-                    <img src={heroImg} alt="hero_img" /></div>
+                <img src={heroImg} alt="hero_img" />
+            </div>
+
+            { /* second section */}
+
+            . <div className="homepage-section2">
+                <img src={section2Img} alt="section2Img" />
+                <div>
+                    <h1>A new way to explore the world </h1>
+                    <p>For decades travellers have reached for Lonely Planet books when looking to plan and execute their perfect
+trip, but now, they can also let Lonely Planet Experiences lead the way</p>
+                    <button>Learn more</button>
+                </div>
+            </div>
         </div>
     );
 };
