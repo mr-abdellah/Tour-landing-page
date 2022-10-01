96aa2e4d02fd9de39ffc796271c07b6c342ea1e8
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-01 16:05:33 +0100

    add responsive featured destinations section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,41 @@
+import { Link } from "react-router-dom";
 import heroImg from "../assets/hero.svg";
-import section2Img from '../assets/section2.svg'
-import '../styles/homepage.css'
+import section2Img from '../assets/section2.svg';
+import DestinationCard from "../components/DestinationCard";
+import '../styles/homepage.css';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+//Importing images
+import des1 from '../assets/destination1.png';
+import des2 from '../assets/destination2.png';
+import des3 from '../assets/destination3.png';
+import des4 from '../assets/destination4.png';
+//
 
-const Homepage = () => {
+const Homepage = () =>
+{
+
+    const data = [
+        {
+            country: 'Indonesia',
+            city: 'Raja Ampat',
+            img: des1
+        },
+        {
+            country: 'China',
+            city: 'Fanjingshan',
+            img: des2
+        },
+        {
+            country: 'Switzerland',
+            city: 'Vevey',
+            img: des3
+        },
+        {
+            country: 'Montenegro',
+            city: 'Skadar',
+            img: des4
+        },
+    ];
 
     return (
         <div className="homepage">
@@ -28,18 +61,32 @@ const Homepage = () => {
                     </form>
                     <button>Explore</button>
                 </div>
-                <img src={heroImg} alt="hero_img" />
+                <img src={ heroImg } alt="hero_img" />
             </div>
 
-            { /* second section */}
+            { /* second section */ }
 
             . <div className="homepage-section2">
-                <img src={section2Img} alt="section2Img" />
+                <img src={ section2Img } alt="section2Img" />
                 <div>
                     <h1>A new way to explore the world </h1>
                     <p>For decades travellers have reached for Lonely Planet books when looking to plan and execute their perfect
-trip, but now, they can also let Lonely Planet Experiences lead the way</p>
+                        trip, but now, they can also let Lonely Planet Experiences lead the way</p>
                     <button>Learn more</button>
+                </div>
+            </div>
+
+            {/* best destinations section */ }
+
+            <div className="homepage-bd">
+                <div className="homepage-bd-title">
+                    <h3>Featured destinations</h3>
+                    <Link to='/destinations'>View all <MdKeyboardArrowRight /></Link>
+                </div>
+
+                <div className="homepage-featured-destinations">
+                    { data.map( card => <DestinationCard img={ card.img } country={ card.country } city={ card.city} />
+                    ) }
                 </div>
             </div>
         </div>
