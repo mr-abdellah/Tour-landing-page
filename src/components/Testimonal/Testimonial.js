fbd31ad60607f351f1dce3fc70e26fbf4f955b35
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-02 11:34:45 +0100

    Add responsive testimonials

--- src/components/Testimonal/Testimonial.js
+++ src/components/Testimonal/Testimonial.js
@@ -1,28 +1,44 @@
-import { Rating } from '@mui/material';
-import testimonialImg from '../../assets/testimonial.svg';
-import './testimonial.css';
-import { BiRightArrowCircle, BiLeftArrowCircle } from 'react-icons/bi'
+import { Rating } from "@mui/material";
+import testimonalImg from "../../assets/testimonial.svg";
+import "./testimonial.css";
+import { BiRightArrowCircle, BiLeftArrowCircle } from "react-icons/bi";
 
-const Testimonial = () =>
-{
-    return (
-        <div className="testimonial">
-            <div className='testimonial_info'>
-                <Rating name="read-only" value={ 5 } readOnly />
-                <p>“Quisque in lacus a urna fermentum euismod. Integer mi nibh, dapibus ac scelerisque eu, facilisis quis purus. Morbi blandit sit amet turpis nec”</p>
-                <h4>Edward Newgate</h4>
-                <p>Founder Circle</p>
-            </div>
-            <div className='testimonial_img'>
-                <img src={ testimonialImg } alt="testimontal" />
-                <div className="testimonial_btns">
-                    <button><BiLeftArrowCircle /></button>
-                    <button><BiRightArrowCircle /></button>
+const Testimonial = () => {
+  return (
+    <div className="testimonial">
+      <p>
+        “Quisque in lacus a urna fermentum euismod. Integer mi nibh, dapibus ac
+        scelerisque eu, facilisis quis purus. Morbi blandit sit amet turpis nec”
+      </p>
 
-                </div>
-            </div>
+      <div className="testimonial_user">
+        <div className="testimonial_user_info">
+          <h3>Edward Newgate</h3>
+          <p>Founder Circle</p>
+          <Rating name="read-only" value={5} readOnly />
         </div>
-    );
+        <div className="testimonial_user_img">
+          <img src={testimonalImg} alt="testimonal" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonial;
+
+// <div className="testimonial">
+//     <div className='testimonial_info'>
+//         <Rating name="read-only" value={ 5 } readOnly />
+//         <p>“Quisque in lacus a urna fermentum euismod. Integer mi nibh, dapibus ac scelerisque eu, facilisis quis purus. Morbi blandit sit amet turpis nec”</p>
+//         <h4>Edward Newgate</h4>
+//         <p>Founder Circle</p>
+//     </div>
+//     <div className='testimonial_img'>
+//         <img src={ testimonalImg } alt="testimontal" />
+//         <div className="testimonial_btns">
+//             <button><BiLeftArrowCircle /></button>
+//             <button><BiRightArrowCircle /></button>
+//         </div>
+//     </div>
+// </div>;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -40,77 +40,86 @@ const Homepage = () =>
     ];
 
     return (
-        <div className="homepage">
-            <div className="homepage-hero">
-                <div className="homepage-hero-wrapper">
-                    <h2>Explore and Travel</h2>
-                    <p>Holiday finder</p>
-                    <div className="line" />
+      <div className="homepage">
+        <div className="homepage-hero">
+          <div className="homepage-hero-wrapper">
+            <h2>Explore and Travel</h2>
+            <p>Holiday finder</p>
+            <div className="line" />
 
-                    <form className="inputs">
-                        <select name="activity"  >
-                            <option value="activity">Activity</option>
-                        </select>
-                        <select name="location" >
-                            <option value="location">Location</option>
-                        </select>
-                        <select >
-                            <option value="age">Age</option>
-                        </select>
-                        <select >
-                            <option value="">11/10/2023</option>
-                        </select>
-                    </form>
-                    <button>Explore</button>
-                </div>
-                <img src={ heroImg } alt="hero_img" />
-            </div>
-
-            { /* second section */ }
-
-            . <div className="homepage-section2">
-                <img src={ section2Img } alt="section2Img" />
-                <div>
-                    <h1>A new way to explore the world </h1>
-                    <p>For decades travellers have reached for Lonely Planet books when looking to plan and execute their perfect
-                        trip, but now, they can also let Lonely Planet Experiences lead the way</p>
-                    <button>Learn more</button>
-                </div>
-            </div>
-
-            {/* best destinations section */ }
-
-            <div className="homepage-bd">
-                <div className="homepage-bd-title">
-                    <h3>Featured destinations</h3>
-                    <Link to='/destinations'>View all <MdKeyboardArrowRight /></Link>
-                </div>
-
-                <div className="homepage-featured-destinations">
-                    { data.map( card => <DestinationCard img={ card.img } country={ card.country } city={ card.city } />
-                    ) }
-                </div>
-            </div>
-
-            {/* */ }
-
-            <div className="guide-section">
-                <div>
-                    <h2>Guides by Thousand Sunny</h2>
-                    <p>Packed with tips and advice from our on-the-ground experts, our city guides app (iOS and Android) is the ultimate resource before and during a trip.</p>
-                    <button>Download</button>
-                </div>
-                <img src={ section3Img } alt="section3" />
-            </div>
-
-            {/* */ }
-
-            <div className="testimonials">
-                <h2>Testimonials</h2>
-                <Testimonial />
-            </div>
-
+            <form className="inputs">
+              <select name="activity">
+                <option value="activity">Activity</option>
+              </select>
+              <select name="location">
+                <option value="location">Location</option>
+              </select>
+              <select>
+                <option value="age">Age</option>
+              </select>
+              <select>
+                <option value="">11/10/2023</option>
+              </select>
+            </form>
+            <button>Explore</button>
+          </div>
+          <img src={heroImg} alt="hero_img" />
         </div>
+        {/* second section */}.{" "}
+        <div className="homepage-section2">
+          <img src={section2Img} alt="section2Img" />
+          <div>
+            <h1>A new way to explore the world </h1>
+            <p>
+              For decades travellers have reached for Lonely Planet books when
+              looking to plan and execute their perfect trip, but now, they can
+              also let Lonely Planet Experiences lead the way
+            </p>
+            <button>Learn more</button>
+          </div>
+        </div>
+        {/* best destinations section */}
+        <div className="homepage-bd">
+          <div className="homepage-bd-title">
+            <h3>Featured destinations</h3>
+            <Link to="/destinations">
+              View all <MdKeyboardArrowRight />
+            </Link>
+          </div>
+
+          <div className="homepage-featured-destinations">
+            {data.map((card) => (
+              <DestinationCard
+                img={card.img}
+                country={card.country}
+                city={card.city}
+              />
+            ))}
+          </div>
+        </div>
+        {/* */}
+        <div className="guide-section">
+          <div>
+            <h2>Guides by Thousand Sunny</h2>
+            <p>
+              Packed with tips and advice from our on-the-ground experts, our
+              city guides app (iOS and Android) is the ultimate resource before
+              and during a trip.
+            </p>
+            <button>Download</button>
+          </div>
+          <img src={section3Img} alt="section3" />
+        </div>
+        {/* */}
+        <div className="testimonials">
+          <h2>Testimonials</h2>
+          <div className="testimonials_container">
+            <Testimonial />
+            <Testimonial />
+            <Testimonial />
+          </div>
+        </div>
+      </div>
     );
 };
 
